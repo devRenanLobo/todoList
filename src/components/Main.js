0ddad3aaf17c71130bc276cfe4c783c517d418cf
Renan Lobo <renanlobo.dev@gmail.com>
2024-10-26 13:40:12 -0300

    feat: css changes and img

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Form from './Form/Form';
 import Tasks from './Tasks/Tasks';
+import image from '../assets/images/hand.png';
 
 import './Main.css'
 
 
 export default function Main(){
     return (
-        <div>
-            <span>Todo List.</span>
+        <div className='mainDiv'>
+            <div className='titleWrap'>
+                <span>Todo List.</span>
+                <img src={image} alt="logo"/>
+            </div>
             <Form></Form>
             <Tasks></Tasks>
         </div>
